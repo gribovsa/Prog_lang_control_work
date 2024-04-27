0e7131dd7752ca916f71beba61b9e7d7a22eaafe
Gribov Sergey <gribovsa@list.ru>
2024-04-27 12:44:54 +0300

    Внёс изменения в метод определения длинны нового массива

--- control_work/Program.cs
+++ control_work/Program.cs
@@ -1,66 +1,58 @@
-﻿// Задача: Написать программу, которая из имеющегося массива строк формирует новый массив из строк, длина которых меньше, либо равна 3 символам. Первоначальный массив можно ввести с клавиатуры, либо задать на старте выполнения алгоритма. При решении не рекомендуется пользоваться коллекциями, лучше обойтись исключительно массивами.
+﻿// Задача: Написать программу, которая из имеющегося массива строк формирует новый массив из строк, длина которых меньше, либо равна 3 символам. 
+// Первоначальный массив можно ввести с клавиатуры, либо задать на старте выполнения алгоритма.
+// При решении не рекомендуется пользоваться коллекциями, лучше обойтись исключительно массивами.
 
 // Примеры:
 // [“Hello”, “2”, “world”, “:-)”] → [“2”, “:-)”]
 // [“1234”, “1567”, “-2”, “computer science”] → [“-2”]
 // [“Russia”, “Denmark”, “Kazan”] → []
 
-// int[] NewArray(int countOfElem)
-// {
-//     int[] newArray = new int[countOfElem];
-//     Random rnd1 = new Random();
-//     for (int i = 0; i < countOfElem; i++)
-//     {
-//         newArray[i] = rnd1.Next(1, 100);
-//     }
-//     return newArray;
-// }
 
-// int[] array = new int[] { -5, -4, -3, -4, -1, 0, 1, 7, 8, 9 };
-// foreach (int item in array)
-// {
-//     Console.Write($"{item * (-1)}");
-
-// }
-
-void PrintArray(string[] arr) //метод печати архивак
-{
+int LenNewArray(string[] arr)
+{    //метод считает длинну нового массива из элементов менее 3 символов
+    int lenNewArr = 0;
     for (int i = 0; i < arr.Length; i++)
     {
-        Console.Write($"{arr[i]} ");
-        
-    }
-    Console.WriteLine();
-}
-
-
-int LenNewArr(string[] arr) //метод определяет длинну нового массива, равную колву слов с длинной  меньше равно 3 символам
-{
-    int len = 0;
-for (int i = 0; i < arr.Length; i++)
-{
-    string word = arr[i];
-    if (word.Length <= 3)
+        if (arr[i].Length <= 3)
         {
-        //Console.Write($"{word} ");
-        len++;
+            lenNewArr++;
         }
+    }
+    return lenNewArr;
+
 }
-return (len);
+
+string[] NewArray(string[] arr, int lenNewArr) //метод созаёт новый массив из элементов старого
+{
+    string[] newArray = new string[lenNewArr]; //создаю новый пустой массив известной длинны
+    int j = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i].Length <= 3) //при выполнении условия
+        {
+            newArray[j] = arr[i]; //заполняю его элементами
+            j = j + 1;
+            //newArray = newArray.Append(arr[i]).ToArray(); //возможен такой вариант
+            //newArray = newArray.Concat(new string[] { arr[i] }).ToArray(); //возможен такой вариант
+        }
+
+    }
+return newArray;
+}
+
+void ShowArray(string[] arrayToShow)
+{
+    for (int i = 0; i < arrayToShow.Length; i++)
+    {
+        Console.WriteLine($"элемент: {arrayToShow[i]} ");
+    }
 }
 
 string[] inputArray = new string[] { "Hello", "2", "world", ":-)" };
-PrintArray(inputArray);
-//LenNewArr(inputArray);
-Console.Write($"Длинна нового массива будет: {LenNewArr(inputArray)}");
+int len =  LenNewArray(inputArray);
+string[]newArr = NewArray(inputArray, len);
+Console.WriteLine("Старый массив");
+ShowArray(inputArray);
+Console.WriteLine("Новый массив");
+ShowArray(newArr);
 
-
-
-
-
-
-// for (int i = 0; i < inputArray.Length; i++)
-// {
-
-//     Console.Write($"{inputArray[i]} ");
-// }
